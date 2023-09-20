6929730b6885c742b8c8fb8dca7403557426808c
Seb-C89 <60698133+Seb-C89@users.noreply.github.com>
2023-09-20 10:20:26 +0200

    init

---
+++ test.js
@@ -0,0 +1,31 @@
+/*class Animal {
+    name:String;
+    constructor(name:String){
+        this.name = name;
+    }
+}
+
+class bidule {
+    private zozo:String;
+    constructor(name:String){
+        this.zozo = name;
+    }
+    setname(name:String){
+        this.zozo = name;
+    }
+}
+
+let nom = "string";
+
+let a:Animal = new Animal("seb");
+    a.name = nom;
+let z:bidule = new bidule("zozo");
+    z.setname("zob");
+
+nom = "gggggggggg";
+
+console.log(a.name);
+console.log(nom);*/
+console.log(-17 % 6);
+console.log(2 % 6);
+console.log(8 % 6);
